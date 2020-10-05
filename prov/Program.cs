bf6e74fc2b3ef3c606128752208b8bb3ded86044
Tyra <ekstrandtyra@gmail.com>
2020-10-05 09:20:45 +0200

    first commit

---
+++ prov/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string svar;
+
+            Console.WriteLine("Välkommen till sänka skepp!");
+            Console.WriteLine("Skriv in en koordinat mellan 1 och 10");
+
+            svar = Console.ReadLine();
+
+            if (svar == "1" || svar == "2" || svar == "3" || svar == "4" || svar == "10")
+            {
+                Console.WriteLine("Miss");
+            }
+            else if (svar == "5" || svar == "6" || svar == "8" || svar == "9")
+            {
+                Console.WriteLine("Near miss");
+            }
+            else if (svar == "7")
+            {
+                Console.WriteLine("Hit!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
